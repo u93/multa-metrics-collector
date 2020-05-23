9e67c8c5bd80cab453c1528609e7388c33315d52
Eugenio <eebf1993@gmail.com>
2020-05-23 01:24:14 -0400

    #8 #9
    - Add Plans Loaddata function & Load Plan Data in BE
    - Add Plans Lambda Handler function

---
+++ src/utils/handlers/aws.py
@@ -0,0 +1,22 @@
+import json
+import traceback
+
+import boto3
+
+from settings.logs import Logger
+
+logs_handler = Logger()
+logger = logs_handler.logger
+
+
+def get_object_from_bucket(bucket_key: str, environment: str, bucket_value: str):
+    try:
+        s3 = boto3.resource("s3")
+        content_object = s3.Object(bucket_key, f"{environment}/{bucket_value}.json")
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
+        return json_content
+    except Exception:
+        logger.error(f"Error getting {environment}/{bucket_value} object")
+        logger.error(traceback.format_exc())
+        return False
--- src/utils/test.py
+++ src/utils/test.py
@@ -2,26 +2,24 @@ import pynamodb
 
 from src.utils.handlers.models import Plans
 
-
-test_plan = Plans(name="test", conditions=["test1", "test2"])
-print(test_plan.id)
-print(test_plan.name)
-print(test_plan.conditions)
-print(test_plan.last_updated)
-
 print("PLAN CREATE")
-Plans.create(name="test-create5", conditions=["test-create-3", "test-create-2"])
+Plans.create(name="test-create7", conditions=["test-create-3", "test-create-2"])
 
 print("PLAN UPDATE BY ID (CREATE FUNCTION)")
-Plans.create(id_="b12fff30-957b-409e-a6b8-50a9e7f26f90", name="test-create5", conditions=["test-create-3", "test-create-2"])
+Plans.create(name="test-create6", conditions=["test-create-3", "test-create-2"])
 
 print("ALL PLANS SCAN")
-plan_records, total_records = Plans.get_records()
+plan_records, total_records, last_evaluated_key = Plans.get_records()
 print(total_records)
+print(last_evaluated_key)
 for plan in plan_records:
     print(plan.to_dict())
+    # plan.delete_record()
 
 print("PLANS QUERY BY ID")
 plan_by_id = Plans.get_record_by_id(id_="8e46f619-610e-4e65-812a-d42b884197f4")
 for plan in plan_by_id:
     print(plan.to_dict())
+
+
+
